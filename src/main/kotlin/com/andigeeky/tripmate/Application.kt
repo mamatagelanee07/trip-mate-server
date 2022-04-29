593196298705a57b14669664ac09a25d2ad11798
mamatagelanee07 <gelaneem@email.com>
2022-04-29 21:29:05 +0100

    fix compile issues due to import missing

--- src/main/kotlin/com/andigeeky/tripmate/Application.kt
+++ src/main/kotlin/com/andigeeky/tripmate/Application.kt
@@ -4,7 +4,7 @@ import com.andigeeky.tripmate.routes.registerRoutes
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.*
+import io.ktor.server.plugins.contentnegotiation.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
