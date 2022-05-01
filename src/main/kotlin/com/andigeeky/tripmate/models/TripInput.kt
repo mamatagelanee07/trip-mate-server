7be17f0d927881cd7557c98e29fc9733dff7fc83
mamatagelanee07 <gelaneem@email.com>
2022-05-01 23:27:27 +0100

    use json serial name for getting trip from input

--- src/main/kotlin/com/andigeeky/tripmate/models/TripInput.kt
+++ src/main/kotlin/com/andigeeky/tripmate/models/TripInput.kt
@@ -1,8 +1,10 @@
 package com.andigeeky.tripmate.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+@SerialName(value = "trip")
 data class TripInput(
     val name: String,
     val startDate: String,
