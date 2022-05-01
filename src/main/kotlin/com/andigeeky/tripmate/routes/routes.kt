252cbdc720f54581186ebfce95d5fe738d5c9607
mamatagelanee07 <gelaneem@email.com>
2022-05-01 21:53:21 +0100

    Changed json response to use camelCase instead of snake_case as our other systems are in mostly js or java

--- src/main/kotlin/com/andigeeky/tripmate/routes/routes.kt
+++ src/main/kotlin/com/andigeeky/tripmate/routes/routes.kt
@@ -2,8 +2,6 @@ package com.andigeeky.tripmate.routes
 
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-
-
 fun Application.registerRoutes() {
     routing {
         tripRouting()
